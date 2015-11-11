fad30fa1e6e18344ad6aa29d5eccf113c93fc068
ShaiLaric <slaric@slaric.com>
2015-11-11 17:34:35 -0500

    Add rename_files.py; update break_time.py

--- break_time.py
+++ break_time.py
@@ -6,12 +6,12 @@
 
 # Use this space to describe your approach to the problem.
 # 1. When user logs on, start a counter.
-# 2. While counter is >= 3:
+# 2. While counter is < 3:
 #    2a. Start a timer that counts 120 minutes
 #	 2b. At 120 minutes, call a "take_break()" function
 # 3. take_break() function:
 #    3a. Display a "Take a break" message
-#    3b. Opern a browser window; link to the appropriate video
+#    3b. Open a browser window; link to the appropriate video
 #    3c. Potentially, close window once video is done?
 #    3d. Return that break is over
 # 4. Increment the counter
@@ -19,4 +19,17 @@
 # Thought: Add an option to warn that a break is approaching and
 # allow user to postpone the break by some amount of time.
 
-# Your code here.
+import time
+import webbrowser
+
+def take_break():
+    breaks = 0
+    
+    print "This program started at " + time.ctime()
+    while breaks < 3:
+        time.sleep(5)
+        print "It's " + time.ctime() + ", so take a break!"
+        webbrowser.open("https://www.youtube.com/watch?v=B5xUiayK-Pc")
+        breaks += 1
+
+take_break()
---
+++ rename_files.py
@@ -0,0 +1,16 @@
+# Lesson 3.2: Use Functions
+# Mini-Project: Secret Message
+
+# Your friend has hidden your keys! To find out where they are,
+# you have to remove all numbers from the files in a folder
+# called prank. But this will be so tedious to do!
+# Get Python to do it for you!
+
+# Use this space to describe your approach to the problem.
+# Step 1: Locate the directory with the files.
+# Step 2: Generate a list of the files.
+# Step 3: Loop through each file in the list.
+# Step 4: In the loop, use a string function to strip out the numbers at the beginning of the name
+# Step 4: Rename the file with the stripped string.
+
+# Your code here.
