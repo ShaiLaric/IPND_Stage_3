7f9232141486a8b528c80d094787a993bac5020c
Shai Laric <slaric@slaric.com>
2015-11-11 11:10:14 -0500

    Added comments with my approach to the problem.

--- break_time.py
+++ break_time.py
@@ -5,9 +5,18 @@
 # once every two hours, a maximum of three times.
 
 # Use this space to describe your approach to the problem.
-#
-#
-#
-#
+# 1. When user logs on, start a counter.
+# 2. While counter is >= 3:
+#    2a. Start a timer that counts 120 minutes
+#	 2b. At 120 minutes, call a "take_break()" function
+# 3. take_break() function:
+#    3a. Display a "Take a break" message
+#    3b. Opern a browser window; link to the appropriate video
+#    3c. Potentially, close window once video is done?
+#    3d. Return that break is over
+# 4. Increment the counter
+# 
+# Thought: Add an option to warn that a break is approaching and
+# allow user to postpone the break by some amount of time.
 
 # Your code here.
